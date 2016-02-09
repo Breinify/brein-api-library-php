e78f6851965cd6cf7d9303f640d346d3180da070
Philipp Meisen <philipp@meisen.net>
2016-02-08 22:22:35 -0800

    added composer

---
+++ tests/libraries/BreinUtilTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Breinify\API\libraries\BreinUtil;
+
+class BreinUtilTest extends PHPUnit_Framework_TestCase {
+
+
+    public function test_that_data_is_set_correctly() {
+        $result = BreinUtil::filterArray([
+            "A" => "a", "a" => "A",
+            "B" => "b", "b" => "B",
+            "C" => "c", "c" => "C",
+            "D" => "d", "d" => "D",
+            "E" => "e", "e" => "E",
+            "F" => "f", "f" => "F",
+            "G" => "g", "g" => "G",
+        ], ["A", "B", "C", "D", "E", "F", "G", "H"]);
+
+        $this->assertArrayHasKey("A", $result);
+        $this->assertArrayHasKey("B", $result);
+        $this->assertArrayHasKey("C", $result);
+        $this->assertArrayHasKey("D", $result);
+        $this->assertArrayHasKey("E", $result);
+        $this->assertArrayHasKey("F", $result);
+        $this->assertArrayHasKey("G", $result);
+        $this->assertEquals(7, count($result));
+    }
+}
