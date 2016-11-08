927e1bb1f7be81475c67322f65d41adf96e93b83
Marco <m.recchioni@me.com>
2016-11-08 21:44:42 +0100

    new files

---
+++ src/BreinTemporalData.php
@@ -0,0 +1,144 @@
+<?php
+namespace Breinify\API;
+
+use Breinify\API\libraries\BreinUtil;
+
+class BreinTemporalData {
+
+    private $apiKey = null;
+    private $secret = null;
+    private $unixTimestamp = null;
+    private $user = null;
+    private $ipAddress = null;
+
+
+    public function __construct() {
+        $this->setUnixTimestamp(null);
+    }
+
+    public function setUser($user) {
+        $sessionId = empty(session_id()) ? null : session_id();
+
+        if (get_class($user) === 'Breinify\API\BreinUser') {
+            /** @noinspection PhpUndefinedMethodInspection */
+            $this->user = $user->data();
+        } else if (get_class($user) === 'WP_User') {
+            $this->user = [
+                'email'     => $user->user_email,
+                'firstName' => $user->user_firstname,
+                'lastName'  => $user->user_lastname,
+                'sessionId' => $sessionId,
+                'signature' => $this->createSignature()
+            ];
+        } else if (is_array($user)) {
+            $this->user = BreinUtil::filterArray($user, BreinUser::$validAttributes);
+        } else {
+            throw new \Exception('Invalid user type: ' . $user);
+        }
+    }
+
+    public function setUnixTimestamp($unixTimestamp) {
+        $this->unixTimestamp = $unixTimestamp == null ? time() : $unixTimestamp;
+    }
+
+    public function getUser() {
+        return $this->user;
+    }
+
+
+    public function setApiKey($apiKey) {
+        $this->apiKey = $apiKey;
+    }
+
+    public function getApiKey() {
+        return $this->apiKey;
+    }
+
+    public function getUnixTimestamp() {
+        return $this->unixTimestamp;
+    }
+
+    public function setSecret($secret) {
+        $this->secret = $secret;
+    }
+
+    /**
+     * @return null
+     */
+    public function getIpAddress() {
+        return $this->ipAddress;
+    }
+
+    /**
+     * @param null $ipAddress
+     */
+    public function setIpAddress($ipAddress) {
+        $this->ipAddress = $ipAddress;
+    }
+
+    public function data() {
+        return [
+            'user'          => $this->user,
+            'apiKey'        => $this->apiKey,
+            'unixTimestamp' => $this->unixTimestamp,
+            'ipAddress'     => (empty($ipAddress) ? '' : $ipAddress)
+        ];
+    }
+
+    /*
+    public function setData($data) {
+
+        // validate the data first
+        if (is_array($data) &&
+            (!empty($data['activities']) || !empty($data['activity'])) &&
+            !empty($data['apiKey']) &&
+            !empty($data['user'])
+        ) {
+            $this->user = $data['user'];
+            $this->activities = (!empty($data['activities']) ? $data['activities'] : [$data['activity']]);
+            $this->apiKey = $data['apiKey'];
+            $this->unixTimestamp = empty($data['unixTimestamp']) ? $this->unixTimestamp : $data['unixTimestamp'];
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+    */
+
+    public function json() {
+        return json_encode($this->data());
+    }
+
+    public function isValid() {
+        return !empty($this->apiKey) &&
+        !empty($this->user) && is_array($this->user) && count($this->user) > 0;
+    }
+
+    /*
+    public function isValid() {
+        return !empty($this->apiKey) &&
+        !empty($this->activities) && is_array($this->activities) && count($this->activities) > 0 &&
+        !empty($this->user) && is_array($this->user) && count($this->user) > 0;
+    }
+    */
+
+    public function createSignature() {
+
+        // TODO
+
+        if (empty($this->secret)) {
+            return null;
+        } else {
+            $activityLength = count($this->activities);
+            $activity = count($this->activities) === 0 ? null : $this->activities[0];
+
+            $message = sprintf("%s%d%d",
+                empty($activity['type']) ? '' : $activity['type'],
+                $this->unixTimestamp,
+                $activityLength);
+
+            return base64_encode(hash_hmac('sha256', $message, $this->secret, true));
+        }
+    }
+}
---
+++ src/BreinUserAdditional.php
@@ -0,0 +1,99 @@
+<?php
+namespace Breinify\API;
+
+class BreinUserAdditional
+{
+
+    public static $validAttributes = [
+        "userAgent", "referrer", "url",
+        "localDateTime", "timezone"
+    ];
+
+    private $userAgent = null;
+    private $referrer = null;
+    private $url = null;
+    private $localDateTime = null;
+    private $timezone = null;
+
+    public function data()
+    {
+        return [
+            'userAgent'     => $this->userAgent,
+            'referrer'      => $this->referrer,
+            'url'           => $this->url,
+            'localDateTime' => $this->localDateTime,
+            'timezone'      => $this->timezone
+        ];
+    }
+
+    /**
+     * @return null
+     */
+    public function getUserAgent() {
+        return $this->userAgent;
+    }
+
+    /**
+     * @param null $userAgent
+     */
+    public function setUserAgent($userAgent) {
+        $this->userAgent = $userAgent;
+    }
+
+    /**
+     * @return null
+     */
+    public function getReferrer() {
+        return $this->referrer;
+    }
+
+    /**
+     * @param null $referrer
+     */
+    public function setReferrer($referrer) {
+        $this->referrer = $referrer;
+    }
+
+    /**
+     * @return null
+     */
+    public function getUrl() {
+        return $this->url;
+    }
+
+    /**
+     * @param null $url
+     */
+    public function setUrl($url) {
+        $this->url = $url;
+    }
+
+    /**
+     * @return null
+     */
+    public function getLocalDateTime() {
+        return $this->localDateTime;
+    }
+
+    /**
+     * @param null $localDateTime
+     */
+    public function setLocalDateTime($localDateTime) {
+        $this->localDateTime = $localDateTime;
+    }
+
+    /**
+     * @return null
+     */
+    public function getTimezone() {
+        return $this->timezone;
+    }
+
+    /**
+     * @param null $timezone
+     */
+    public function setTimezone($timezone) {
+        $this->timezone = $timezone;
+    }
+
+}
